85e2825811c8c2c58797de2ff9085c7ae0fd574a
Jonathan Rainer <jr776@york.ac.uk>
2018-08-21 14:46:20 +0100

    Adding setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Shiji",
+    version="1.0",
+    description="Tool to automate generating simulation testbenches for RI5CY",
+    author="Jonathan Rainer",
+    author_email="jonathan.rainer@york.ac.uk",
+    packages=["shiji"]
+)
